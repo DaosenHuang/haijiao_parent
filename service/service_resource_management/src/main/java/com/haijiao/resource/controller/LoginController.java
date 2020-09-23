96e690ab93c8f11ac5da5600ee76b134cf28b839
HuangYu <2442453418@qq.com>
2020-09-23 10:14:23 +0800

    添加登录接口

--- service/service_resource_management/src/main/java/com/haijiao/resource/controller/LoginController.java
+++ service/service_resource_management/src/main/java/com/haijiao/resource/controller/LoginController.java
@@ -15,7 +15,7 @@ public class LoginController {
         return Result.ok().data("token", "admin");
     }
 
-    //info 2020-09-23
+    //info 2020-09-23哈哈
     @GetMapping("info")
     public Result info(){
         return Result.ok().data("roles","[admin]").data("avatar", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600789647565&di=0cfe0118b40e118fb16e783ceda94794&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201410%2F09%2F20141009224754_AswrQ.jpeg");
