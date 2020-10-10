5d51a1ffa327e9d5d4599131f8dbf77ee7f1027a
HuangYu <2442453418@qq.com>
2020-10-10 10:38:23 +0800

    添加自动生成表模块

--- service/project_management/src/main/java/com/haijiao/resource/ProjectApplication.java
+++ service/project_management/src/main/java/com/haijiao/resource/ProjectApplication.java
@@ -2,6 +2,7 @@ package com.haijiao.resource;
 
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.haijiao"})
+@ComponentScan(basePackages={"com.haijiao" })
+
 public class ProjectApplication {
     public static void main(String[] args){
         SpringApplication.run(ProjectApplication.class, args);
